5a24ed10a4b0329a3cd28aa93614245ec9428a0b
Nkaleth <nseguralu@gmail.com>
2023-02-15 15:42:11 -0500

    CHANGES: according last review , style and added children

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Calculator.css';
+import { ButtonNumbers, ButtonOperator } from './Button';
 
 function Calculator() {
   return (
@@ -8,30 +9,31 @@ function Calculator() {
       <section className="Totalbuttons">
         <div className="ButtonsCont">
           <section className="Numbers">
-            <button className="ButtonClass" type="button">AC</button>
-            <button className="ButtonClass" type="button">+/-</button>
-            <button className="ButtonClass" type="button">%</button>
-            <button className="ButtonClass" type="button">7</button>
-            <button className="ButtonClass" type="button">8</button>
-            <button className="ButtonClass" type="button">9</button>
-            <button className="ButtonClass" type="button">4</button>
-            <button className="ButtonClass" type="button">5</button>
-            <button className="ButtonClass" type="button">6</button>
-            <button className="ButtonClass" type="button">1</button>
-            <button className="ButtonClass" type="button">2</button>
-            <button className="ButtonClass" type="button">3</button>
+            <ButtonNumbers text="AC" />
+            <ButtonNumbers text="+/-" />
+            <ButtonNumbers text="%" />
+            <ButtonNumbers text="7" />
+            <ButtonNumbers text="8" />
+            <ButtonNumbers text="9" />
+            <ButtonNumbers text="4" />
+            <ButtonNumbers text="5" />
+            <ButtonNumbers text="6" />
+            <ButtonNumbers text="1" />
+            <ButtonNumbers text="2" />
+            <ButtonNumbers text="3" />
           </section>
           <div className="lastrowNum">
-            <button className="ButtonClass" type="button">0</button>
-            <button className="ButtonClass" type="button">.</button>
+            <ButtonNumbers text="0" />
+            <ButtonNumbers text="." />
           </div>
         </div>
         <section className="Operators">
-          <button className="ButtonClassOper" type="button">÷</button>
-          <button className="ButtonClassOper" type="button">x</button>
-          <button className="ButtonClassOper" type="button">-</button>
-          <button className="ButtonClassOper" type="button">+</button>
-          <button className="ButtonClassOper" type="button">=</button>
+
+          <ButtonOperator text="÷" />
+          <ButtonOperator text="x" />
+          <ButtonOperator text="-" />
+          <ButtonOperator text="+" />
+          <ButtonOperator text="=" />
         </section>
       </section>
     </div>
